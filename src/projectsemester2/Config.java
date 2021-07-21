acae2179aaa840ba402f74f2d0b6a261d4910a58
hidayatul02 <hidayatul20ti@mahasiswa.pcr.ac.id>
2021-07-21 20:37:16 +0700

    commit hidayatul02

---
+++ src/projectsemester2/Config.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectsemester2;
+
+/**
+ *
+ * @author robby
+ */
+
+import java.sql.*;
+
+
+public class Config {
+    private static Connection koneksi;
+    public static Connection getKoneksi(){
+        if (koneksi == null){
+          try {
+            String url = "jdbc:mysql://localhost:3306/mini_market"; // nama databasenya login
+            String user="root"; // username mysql root
+            String pass=""; //passwordnya kosong
+
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            koneksi = DriverManager.getConnection(url , user, pass);
+
+            }catch(SQLException t){
+                    System.out.print("Gagal melakukan koneksi ke database");
+                }
+            }
+
+            return koneksi;
+        }
+    } 
